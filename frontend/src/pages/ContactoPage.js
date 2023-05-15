3e6bb36845207253748b7b1570d1de3f4ec3ea90
Martin Cornelio Piscitelli <mcp314@gmail.com>
2023-05-14 23:36:16 -0300

    version2

--- frontend/src/pages/ContactoPage.js
+++ frontend/src/pages/ContactoPage.js
@@ -1,12 +1,17 @@
 import React from "react";
 import '../styles/components/pages/ContactoPage.css';
+import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaMailBulk } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+
 
 const ContactoPage = (props) =>{
    return (
      <main className="holder contacto">
       <div>
         <h2>Contacto Rápido</h2>
-        <form action="" method="" className="formularo">
+        <form action="" method="" className="formulario">
           <p>
             <label for="nombre"> Nombre</label>
             <input type="text" name="" />
@@ -31,10 +36,10 @@ const ContactoPage = (props) =>{
         <h2>Otras vias de comunicación</h2>
         <p>También puede contactarse con nosotros usando los siguientes medios</p>
         <ul>
-          <li>Telefono: 12345678</li>
-          <li>Email: contacto@transportesx.com.ar</li>
-          <li>Facebook:</li>
-          <li>Twitter:</li>
+          <li><FaMailBulk  /><a href="mailto:mcp314@gmail.com">mail: contacto@colibri.com.ar</a></li>
+          <li><FaWhatsapp /><a href="https://wa.me/2215430858/">2215430858</a></li>
+          <li><FaInstagram  /><a href="">LookAgain</a></li>
+          <li><FaFacebookSquare  /><a href="">LookAgain</a></li>
         </ul>
       </div>
      </main>
--- frontend/src/pages/NosotrosPage.js
+++ frontend/src/pages/NosotrosPage.js
@@ -8,6 +8,7 @@ const NosotrosPage = (props) =>{
         <h2>Historia</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Duis aliquet ornare convallis. Phasellus porta nunc non sapien facilisis semper. Ut auctor in nisl id ultrices. In finibus diam ac nisi facilisis pulvinar. Vestibulum magna ipsum, luctus ac diam non, pulvinar elementum sem. Maecenas vitae velit libero. Integer quam quam, pretium et mi vel, faucibus sollicitudin lectus. Phasellus lectus ex, pretium tempus maximus sed, auctor in orci. Curabitur ullamcorper vulputate enim, sed feugiat lacus imperdiet non. Vestibulum vitae efficitur nisl. Maecenas pharetra erat vitae sapien convallis, et ultricies nibh convallis. Nunc condimentum et dolor eu gravida. Integer eu nibh ac est sodales aliquet. Proin fermentum iaculis risus vel luctus. Mauris ut mi at purus tristique eleifend eu nec erat.</p>
       </div>
       <div className="staff">
         <h2>Staff</h2>
@@ -16,6 +17,24 @@ const NosotrosPage = (props) =>{
             <img src="images/nosotros.png" alt="Juan Gomez" />
             <h5>Juan Gomez</h5>
             <h6>Gerente General</h6>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div className="persona">
+            <img src="images/nosotros2.png" alt="Jane Doe" />
+            <h5>Jane Doe</h5>
+            <h6>Asistente Comercial</h6>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div className="persona">
+            <img src="images/nosotros3.png" alt="Maria Zarasa" />
+            <h5>Maria Zarasa</h5>
+            <h6>Logística</h6>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div className="persona">
+            <img src="images/nosotros4.png" alt="Juan Gomez" />
+            <h5>Elsa Bor de Lencuentro</h5>
+            <h6>Atención al público</h6>
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
